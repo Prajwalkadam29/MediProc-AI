2c778378b732c15fd84634cd9e486191b0dd7ffd
Prajwal <160503760+Prajwalkadam29@users.noreply.github.com>
2026-01-01 18:55:47 +0530

    Initial commit

---
+++ backend/src/test/java/com/praj/mediprocess/MediprocessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praj.mediprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediprocessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
